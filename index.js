ebffbfb7694c707acf526b62496e4f826d854d6e
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2024-12-14 15:43:16 +0300

    updated the js file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const myButton = document.getElementById("button");
-
 function addingEventListener() {
-  myButton.addEventListener("click", function () {
-    alert("I was clicked!");
-  });
+  const input = document.getElementById('button');
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  input.addEventListener('click', clickAlert);
 }
 
+// Call the outer function to activate the event listener
 addingEventListener();
